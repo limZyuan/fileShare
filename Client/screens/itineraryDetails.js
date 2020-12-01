24f0c800d10e1db54326c4f5fe073a7d365edcdb
limZyuan <58379837+limZyuan@users.noreply.github.com>
2020-12-01 16:47:16 +0800

    update

--- Client/screens/itineraryDetails.js
+++ Client/screens/itineraryDetails.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {globalStyles} from '../styles/global';
 import Card from '../shared/card';
 import FlatButton from '../shared/button';
 
+// file fetch
+import RNFetchBlob from 'rn-fetch-blob';
+
 const ItineraryDetails = ({navigation}) => {
+  console.log(navigation.getParam('filename'), navigation.getParam('key'));
+  // on modal update, fetch the respective file to load in viewer. File reader option is used to indicate whether the file is fetched for view or for download.
+  useEffect(() => {
+    if (navigation.getParam('filename')) {
+      RNFetchBlob.config({
+        fileCache: true,
+        // // by adding this option, the temp files will have a file extension
+        // appendExt: 'docx',
+      })
+        .fetch(
+          'GET',
+          'https://itiapinodejs.herokuapp.com/files/download?file=3 days in Bali with family.docx&id=5f9bebf90067e045f8c23131&fileReader=true',
+        )
+        .then((res) => {
+          // the temp file path
+          console.log('The file saved to ', res.path());
+        });
+    }
+  }, [navigation]);
+
   return (
     <View style={globalStyles.container}>
       <Card>
