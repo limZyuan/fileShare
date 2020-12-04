5ccb57ec65a884927653f02ab22cad7271382628
limZyuan <58379837+limZyuan@users.noreply.github.com>
2020-12-04 18:24:49 +0800

    formik and yup

--- Client/screens/upload.js
+++ Client/screens/upload.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
 import {globalStyles} from '../styles/global';
 import RNFetchBlob from 'rn-fetch-blob';
 import FlatButton from '../shared/button';
+import {Formik} from 'formik';
+import * as yup from 'yup';
 
 // doc picker wrapper
 import DocumentPicker from 'react-native-document-picker';
+
+// schema for form
+const UploadSchema = yup.object({
+  title: yup.string().required().min(4),
+  body: yup.string().required().min(8),
+  rating: yup
+    .string()
+    .required()
+    .test('is-num-1-5', 'Rating must be a number 1 - 5', (value) => {
+      return parseInt(value) < 6 && parseInt(value) > 0;
+    }),
+});
 
 const uploadFunc = () => {
   // Pick a single file
@@ -57,6 +71,50 @@ const Upload = () => {
   return (
     <View style={globalStyles.container}>
       <Text>Upload Screen</Text>
+      <Formik
+        initialValues={{title: '', body: '', rating: ''}}
+        validationSchema={UploadSchema}
+        onSubmit={(values, actions) => {
+          console.log(values);
+        }}>
+        {(formikProps) => (
+          <View>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Review Title"
+              onChangeText={formikProps.handleChange('title')}
+              value={formikProps.values.title}
+              onBlur={formikProps.handleBlur('title')}
+            />
+            <Text style={globalStyles.errorText}>
+              {formikProps.touched.title && formikProps.errors.title}
+            </Text>
+            <TextInput
+              multiline
+              style={globalStyles.input}
+              placeholder="Review body"
+              onChangeText={formikProps.handleChange('body')}
+              value={formikProps.values.body}
+              onBlur={formikProps.handleBlur('body')}
+            />
+            <Text style={globalStyles.errorText}>
+              {formikProps.touched.body && formikProps.errors.body}
+            </Text>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Rating (1-5)"
+              onChangeText={formikProps.handleChange('rating')}
+              value={formikProps.values.rating}
+              keyboardType="numeric"
+              onBlur={formikProps.handleBlur('rating')}
+            />
+            <Text style={globalStyles.errorText}>
+              {formikProps.touched.rating && formikProps.errors.rating}
+            </Text>
+            <FlatButton text="submit" onPress={formikProps.handleSubmit} />
+          </View>
+        )}
+      </Formik>
       <FlatButton
         style={{marginTop: 16, backgroundColor: '#a5d1e3'}}
         text="Upload"
